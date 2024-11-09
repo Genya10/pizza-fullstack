0caa66dbc51964ba6f011936bbe11164067e75d6
Jenya <113167825+Genya10@users.noreply.github.com>
2024-11-09 12:53:52 +0200

    create index.ts to shared and create button to header with ShoppingCart and ArrowRight

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { Container } from './container'
 import Image from 'next/image'
 import { Button } from '../ui'
+import { UserRound, ShoppingCart , ArrowRight} from 'lucide-react'
 
 interface Props {
     className?: string;
@@ -11,7 +12,7 @@ interface Props {
 export const Header: React.FC<Props> = ({className}) => {
     return (
         <header className={cn('border border-b', className)}>
-        <Container className='flex items-center justify-between py-8'>
+        <Container className='flex items-center justify-around py-8'>
             {/* Left part */}
           <div className='flex items-center gap-4'>
             <Image src='/logo.png' alt='Logo' width={35} height={35}/>
@@ -21,8 +22,22 @@ export const Header: React.FC<Props> = ({className}) => {
             </div>
           </div>
           {/* Right part */}
-          <div className='flex items-center gap-3'>
-           <Button variant='outline'>Enter</Button>
+          <div className='flex items-center gap-3'>            
+           <Button variant='outline' className='flex items-center gap-2'>
+            <UserRound size={16}/>
+              Enter
+            </Button>
+            <div>
+                <Button className='group relative'>
+                  <b>120 $</b>
+                  <span className='h-full w-[2px] bg-white/30 mx-3'/>
+                  <div className='flex items-center gap-1 transition duration-300 group-hover:opacity-0'>
+                   <ShoppingCart size={16} className='relative' strokeWidth={2}/>
+                   <b>3</b>
+                  </div>
+                  <ArrowRight size={20} className='absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0'/>
+                </Button>
+            </div>
           </div>
         </Container>
         </header>
