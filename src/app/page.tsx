64925739fa936b47d6a39140a3e7df23df277cac
Jenya <113167825+Genya10@users.noreply.github.com>
2024-11-16 13:25:19 +0200

    create component products-group-list.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Container, Title, TopBar, Filters, ProductCard } from "@/components/shared";
+import { ProductsGroupList } from "@/components/shared/products-group-list";
 
 export default function Home() {
   return (
@@ -9,7 +10,7 @@ export default function Home() {
      <TopBar className="pl-6 pr-4"/>
      
      <Container className="pb-14 pl-6 mt-8">
-      <div className="flex gap-[60px]">
+      <div className="flex gap-[80px]">
         {/* Filter */}
         <div className="w-[250px]">
          <Filters/>
@@ -17,7 +18,87 @@ export default function Home() {
         {/* List products */}
         <div className="flex-1">
           <div className="flex flex-col gap-16">
-           <ProductCard id={0} name="Margarita" price={70} imageUrl={'https://media.istockphoto.com/id/1168754685/photo/pizza-margarita-with-cheese-top-view-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=psLRwd-hX9R-S_iYU-sihB4Jx2aUlUr26fkVrxGDfNg='}/>
+           <ProductsGroupList 
+              title="Pizzas" 
+              items={[
+               {
+                id:1,
+                name:'Cheses-pizza',
+                imageUrl:'https://media.istockphoto.com/id/1168754685/photo/pizza-margarita-with-cheese-top-view-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=psLRwd-hX9R-S_iYU-sihB4Jx2aUlUr26fkVrxGDfNg=',
+                price: 70,
+                items:[{price:70}]
+               },
+               {
+                id:2,
+                name:'Cheses-pizza',
+                imageUrl:'https://media.istockphoto.com/id/1168754685/photo/pizza-margarita-with-cheese-top-view-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=psLRwd-hX9R-S_iYU-sihB4Jx2aUlUr26fkVrxGDfNg=',
+                price: 70,
+                items:[{price:70}]
+               },
+               {
+                id:3,
+                name:'Cheses-pizza',
+                imageUrl:'https://media.istockphoto.com/id/1168754685/photo/pizza-margarita-with-cheese-top-view-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=psLRwd-hX9R-S_iYU-sihB4Jx2aUlUr26fkVrxGDfNg=',
+                price: 70,
+                items:[{price:70}]
+               },
+               {
+                id:4,
+                name:'Cheses-pizza',
+                imageUrl:'https://media.istockphoto.com/id/1168754685/photo/pizza-margarita-with-cheese-top-view-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=psLRwd-hX9R-S_iYU-sihB4Jx2aUlUr26fkVrxGDfNg=',
+                price: 70,
+                items:[{price:70}]
+               },
+               {
+                id:5,
+                name:'Cheses-pizza',
+                imageUrl:'https://media.istockphoto.com/id/1168754685/photo/pizza-margarita-with-cheese-top-view-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=psLRwd-hX9R-S_iYU-sihB4Jx2aUlUr26fkVrxGDfNg=',
+                price: 70,
+                items:[{price:70}]
+               },
+            ]} 
+              categoryId={1}/>
+
+            <ProductsGroupList 
+              title="Chisburgers" 
+              items={[
+               {
+                id:1,
+                name:'Cheses-pizza',
+                imageUrl:'https://media.istockphoto.com/id/520410807/photo/cheeseburger.jpg?s=612x612&w=0&k=20&c=fG_OrCzR5HkJGI8RXBk76NwxxTasMb1qpTVlEM0oyg4=',
+                price: 70,
+                items:[{price:70}]
+               },
+               {
+                id:2,
+                name:'Cheses-pizza',
+                imageUrl:'https://media.istockphoto.com/id/520410807/photo/cheeseburger.jpg?s=612x612&w=0&k=20&c=fG_OrCzR5HkJGI8RXBk76NwxxTasMb1qpTVlEM0oyg4=',
+                price: 70,
+                items:[{price:70}]
+               },
+               {
+                id:3,
+                name:'Cheses-pizza',
+                imageUrl:'https://media.istockphoto.com/id/520410807/photo/cheeseburger.jpg?s=612x612&w=0&k=20&c=fG_OrCzR5HkJGI8RXBk76NwxxTasMb1qpTVlEM0oyg4=',
+                price: 70,
+                items:[{price:70}]
+               },
+               {
+                id:4,
+                name:'Cheses-pizza',
+                imageUrl:'https://media.istockphoto.com/id/520410807/photo/cheeseburger.jpg?s=612x612&w=0&k=20&c=fG_OrCzR5HkJGI8RXBk76NwxxTasMb1qpTVlEM0oyg4=',
+                price: 70,
+                items:[{price:70}]
+               },
+               {
+                id:5,
+                name:'Cheses-pizza',
+                imageUrl:'https://media.istockphoto.com/id/520410807/photo/cheeseburger.jpg?s=612x612&w=0&k=20&c=fG_OrCzR5HkJGI8RXBk76NwxxTasMb1qpTVlEM0oyg4=',
+                price: 70,
+                items:[{price:70}]
+               },
+            ]} 
+              categoryId={2}/>
           </div>
         </div>
       </div>
--- src/components/shared/product-card.tsx
+++ src/components/shared/product-card.tsx
@@ -12,14 +12,15 @@ interface Props {
 }
 
 export const ProductCard: React.FC<Props> = ({ id, name, price, imageUrl, className}) => {
-  return (
+ 
+    return (
     <div className={className}>
      <Link href={`product/${id}`}>
       <div className="flex justify-center p-6 bg-secondary rounded-lg h-[260px]">
        <img className="w-[215px]" src={imageUrl} alt='Logo'/>
       </div>
       <Title text={name} size="sm" className="mb-1 mt-3 font-bold"/>
-      <p className="text-sm text-gray-400">
+      <p className="text-sm text-gray-600">
        Parmezan, cherry, mozarella, souse, tomato, avocado, garlic
       </p>
       <div className="flex justify-between items-center mt-4">
