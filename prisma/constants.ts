42da208e37198b41a5524d2ad9c82441bf1a0249
Jenya <113167825+Genya10@users.noreply.github.com>
2024-11-25 11:54:02 +0200

    create seed.ts and constants.ts to prisma,install bcrypt

---
+++ prisma/constants.ts
@@ -0,0 +1,17 @@
+export const categories = [
+    {
+        name:'Pizzas'
+    },
+    {
+        name:'Breakfasts'
+    },
+    {
+        name:'Snacks'
+    },
+    {
+        name:'Coctails'
+    },
+    {
+        name:'Drinks'
+    },
+]
---
+++ prisma/seed.ts
@@ -0,0 +1,51 @@
+import { categories } from './constants'
+import { prisma } from './prisma-client'
+import { hashSync } from 'bcrypt'
+
+async function up(){
+ await prisma.user.createMany({
+    data:[
+        {
+            fullName:'User Test',
+            email:'user@test.com',
+            password: hashSync('111111', 10),
+            verified: new Date(),
+            role:'USER'
+        },
+        {
+            fullName:'Admin Test',
+            email:'admin@test.com',
+            password: hashSync('111111', 10),
+            verified: new Date(),
+            role:'ADMIN'
+        }
+    ]
+ })
+
+ await prisma.category.createMany({
+    data:categories
+ })
+}
+
+async function down(){
+    await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`
+}
+
+async function main(){
+    try {
+        await down()
+        await up()
+    } catch(e){
+        console.error(e)
+    }
+}
+
+main()
+.then(async()=> {
+    await prisma.$disconnect()
+})
+.catch(async(e)=> {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+})
