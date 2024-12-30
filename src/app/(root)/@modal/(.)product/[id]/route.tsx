671a395385f05f72e5e42ce10eb19b5931af4c43
Jenya <113167825+Genya10@users.noreply.github.com>
2024-12-30 10:12:05 +0200

    create folder @modal with attended folders and files

---
+++ src/app/(root)/@modal/(.)product/[id]/route.tsx
@@ -0,0 +1,43 @@
+import { notFound } from "next/navigation"
+import { prisma } from "../../../../../../prisma/prisma-client"
+import { Container, ProductImage, Title } from "@/components/shared"
+import { GroupVariants } from "@/components/shared/group-variants"
+
+export default async function ProductModalPage({params: {id}}:{params:{id: string}}) {//?
+  const product = await prisma.product.findFirst({where:{id: Number(id)}})
+
+  if(!product){
+    return notFound()
+  }
+
+  return (
+    <Container className="flex  my-10">
+      <div className="flex flex-1">
+      <ProductImage imageUrl={product.imageUrl} size={40}/>
+      </div>
+
+      <div className="w-[490px] bg-[#FCFCFC] p-7">
+       <Title text={product.name} size="md" className="font-extrabold mb-1"/>
+       <p className="text-gray-400">Lorem Lorem Lorem </p>  
+       <GroupVariants 
+        selectedValue="3"
+         items={[
+          {
+            name:'Little',
+            value: '1'
+          },
+          {
+            name:'Middle',
+            value:'2',
+            disabled: true
+          },
+          {
+            name:'Large',
+            value:'3'
+          }
+         ]}
+       />
+      </div>
+    </Container>
+  )
+}
